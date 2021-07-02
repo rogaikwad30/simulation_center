21e64a567481ded9a9ae88ac1f1cb3f83c746c19
rohan gaikwad <rogaikwad30@gmail.com>
2021-07-03 00:33:58 +0530

    about us section added

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,9 @@
 const navToggler = document.getElementById('nav-toggler')
 const mobNav = document.getElementById('mob-nav')
 const tabNav = document.getElementById('tab-nav')
+const readMore = document.getElementById('more')
+const moreBtn = document.getElementById('more-btn')
+
 
 navToggler.addEventListener('click' , ()=>{
     navToggler.classList.toggle('fa-times') ;
@@ -14,7 +17,10 @@ const navigationResponsiveness = ()=>{
     mobNav.classList.toggle('mob-nav')
 }
 
-
+moreBtn.addEventListener('click' , ()=>{
+    readMore.classList.toggle('show')
+    moreBtn.classList.toggle('hide')
+})
 
 
 
